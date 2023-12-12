e333a3a7da5985e418b428489321b1c41960c31a
Francisco Carvajal <fraco_oxza@outlook.com>
2023-12-12 00:30:28 -0300

    feat(install.sh): move old settings
    Before linking the configurations folders, move the old ones.

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
+mv ~/.config/nvim ~/.config/nvim-old
 ln -s $(pwd)/nvim/ ~/.config/nvim
+
+mv ~/.config/hypr ~/.config/hypr-old
 ln -s $(pwd)/hypr/ ~/.config/hypr
+
+mv ~/.config/fish ~/.config/fish-old
 ln -s $(pwd)/fish/ ~/.config/fish
+
+mv ~/.config/kitty ~/.config/kitty-old
 ln -s $(pwd)/kitty/ ~/.config/kitty
+
+mv ~/.config/waybar ~/.config/waybar
 ln -s $(pwd)/waybar/ ~/.config/waybar
