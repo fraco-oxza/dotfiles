e328770fed8716a1a308d4060ddfa17ee1807fcc
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2024-02-20 14:35:04 -0300

    feat(fish): removed fortune greeter

--- fish/config.fish
+++ fish/config.fish
@@ -9,8 +9,7 @@ set -x EDITOR nvim
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting 
-    fortune -s
-    
+
     zoxide init fish | source
     starship init fish | source
 
