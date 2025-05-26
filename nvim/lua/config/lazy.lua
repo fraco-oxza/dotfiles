2848357af27c96632642512854af6da9062351cc
Francisco Carvajal Ossa <fraco_oxza@outlook.com>
2025-05-26 11:37:24 -0400

    feat: sync

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -58,7 +58,6 @@ wo.wrap = false
 vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
   pattern = { "*.hl", "hypr*.conf" },
   callback = function(event)
-    require("notify")("Hello")
     vim.cmd("setlocal filetype=hyprlang")
   end,
 })
--- nvim/lua/plugins/plantuml.lua
+++ nvim/lua/plugins/plantuml.lua
@@ -2,6 +2,6 @@ return {
   "weirongxu/plantuml-previewer.vim",
   dependencies = { "aklt/plantuml-syntax", "tyru/open-browser.vim" },
   init = function()
-    vim.cmd([[let g:plantuml_previewer#plantuml_jar_path = "/home/fraco/Downloads/plantuml-mit-1.2025.0.jar"]])
+    vim.cmd([[let g:plantuml_previewer#plantuml_jar_path = "/home/fraco/Downloads/plantuml-mit-1.2025.2.jar"]])
   end,
 }
